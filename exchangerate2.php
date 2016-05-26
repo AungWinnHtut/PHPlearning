5dbfd2f6b0a291fae0ec2703d6d00d3b86e969da
AungWinnHtut <aungwinnhtut@gmail.com>
2016-05-26 23:46:33 +0630

    exchangerate2.php

---
+++ exchangerate2.php
@@ -0,0 +1,16 @@
+<?php
+        $USDRate = 1179;
+        $YuanRate = 180;
+        $RupeeRate = 17.61;
+
+        $MMK = 45334588;
+
+        $USD = $MMK / $USDRate;
+        $Yuan = $MMK / $YuanRate;
+        $Rupee = $MMK / $RupeeRate;
+
+        echo "Myanmar Kyat $MMK worths $USD dollars or $Yuan yuans or $Rupee rupees!"
+
+
+
+?>
