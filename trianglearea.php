d29bbc64a55ac77e1eae35ca318d9d87a9aa7974
AungWinnHtut <aungwinnhtut@gmail.com>
2016-05-29 10:16:33 +0630

    trianglearea.php by ko Thawtar Oo

---
+++ trianglearea.php
@@ -0,0 +1,13 @@
+<!-- Answer by ko Thawtar Oo -->
+<!-- 29-5-2016 -->
+<html>
+<body>
+<h1>Area of Triaingle</h1>
+<?php
+$h= 3; 
+$b = 3;
+$a=0.5*$b*$h;
+echo "<br>Area of Trangle is $a";
+?>
+</body>
+</html>
